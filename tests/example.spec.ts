c61c05664fe72024b74627c175d40368a2998561
tone-man <theToneMan@outlook.com>
2023-09-14 21:02:41 -0700

    Testing Run of Workflow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('https://vitejs.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Vite/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-
-  // Click the get started link.
-  await page.getByRole('button').click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('button').innerText()).toMatch(/1/);
 });
