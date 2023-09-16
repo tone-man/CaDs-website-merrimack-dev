a48195e412566f8aa76b47f669fddd6fe1975491
tone-man <theToneMan@outlook.com>
2023-09-16 11:31:04 -0400

    Setup Playwright Boilerplate Tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://vitejs.dev/');
+test('Has Vite in Title', async ({ page }) => {
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Vite/);
+});
+
+test('Button Increments on Press', async({ page }) => {
+  await page.goto('/');
+
+  const button = await page.getByRole('button', {name: 'count'});
+  await button.click();
+
+  await expect( await button.textContent()).toContain('1')
 });
