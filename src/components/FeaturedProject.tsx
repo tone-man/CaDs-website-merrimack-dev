3c48a9f0c6f1d30809642a1939a2348ada83a273
Brianna Van Hecke <vanheckeb@merrimack.edu>
2023-10-16 16:25:03 -0400

    Added carousel of profiles

--- src/components/FeaturedProject.tsx
+++ src/components/FeaturedProject.tsx
@@ -1,25 +1,78 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Carousel, Col, Container, Row, Image, Nav } from 'react-bootstrap';
 
 import '../css/FeaturedProject.css'
+
+import merrimackLogo from '../imgs/logo.webp';
 
 function FeaturedProject() {
     return (
         <>
             <Container fluid className='projects-container container-fluid'>
                 <Row className='project-row '>
-                    <Col md={6} xs={11} className="">
+                    <Col md={6} xs={11}>
                         <Card className='project-card-left'>
+                            <Nav.Link href="#">
+                                <Col md={{ span: 2, offset: 10 }} xs={{ span: 4, offset: 8 }}>
+                                    <Carousel variant='link'>
+                                        <Carousel.Item>
+                                            <div className='mx-auto' style={{ width: '75px', height: '75px' }}>
+                                                <Image
+                                                    className="rounded-circle overflow-hidden"
+                                                    src={merrimackLogo}
+                                                    alt="Profile Picture"
+                                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                                />
+                                            </div>
+                                        </Carousel.Item>
+                                        <Carousel.Item>
+                                            <div className='mx-auto' style={{ width: '75px', height: '75px' }}>
+                                                <Image
+                                                    className="rounded-circle overflow-hidden"
+                                                    src={merrimackLogo}
+                                                    alt="Profile Picture"
+                                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                                />
+                                            </div>
+                                        </Carousel.Item>
+
+                                    </Carousel>
+                                </Col>
+                            </Nav.Link>
                         </Card>
                     </Col>
                 </Row>
                 <Row className='project-row'>
                     <div>
-                        <Col md={{ span: 6, offset: 6 }} xs={{ span: 11, offset: 1}}>
+                        <Col md={{ span: 6, offset: 6 }} xs={{ span: 11, offset: 1 }}>
                             <Card className='project-card-right'>
-                                <h1 > hello</h1>
+                                <Col md={{ span: 2, offset: 0 }} xs={{ span: 4, offset: 0 }}>
+                                    <Carousel variant='link'>
+                                        <Carousel.Item>
+                                            <div className='mx-auto' style={{ width: '75px', height: '75px' }}>
+                                                <Image
+                                                    className="rounded-circle overflow-hidden"
+                                                    src={merrimackLogo}
+                                                    alt="Profile Picture"
+                                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                                />
+                                            </div>
+                                        </Carousel.Item>
+                                        <Carousel.Item>
+                                            <div className='mx-auto' style={{ width: '75px', height: '75px' }}>
+                                                <Image
+                                                    className="rounded-circle overflow-hidden"
+                                                    src={merrimackLogo}
+                                                    alt="Profile Picture"
+                                                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                                                />
+                                            </div>
+                                        </Carousel.Item>
+
+                                    </Carousel>
+                                </Col>
                             </Card>
                         </Col>
                     </div>
