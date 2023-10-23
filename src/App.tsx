e71cddd2d64d973c1c493a0f4fcb48066cc33096
Brianna Van Hecke <vanheckeb@merrimack.edu>
2023-10-22 20:58:16 -0400

    Added tests back to avoid "no tests" error

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import ProjectList from './components/ProjectList';
@@ -5,6 +7,7 @@ import Header from './components/Header';
 import EventsCarousel from './components/EventsCarousel';
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -13,6 +16,21 @@ function App() {
       <ProjectList />
       <EventsCarousel />
 
+      <Footer />
+      <h1>Vite + React</h1>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
       <Footer />
     </>
   )
