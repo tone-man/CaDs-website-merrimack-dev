41d427506eaaa7ab71653623c42a7ff857db5f4c
Brianna Van Hecke <vanheckeb@merrimack.edu>
2023-10-26 18:26:33 -0400

    Fixed whitelist section

--- src/components/RequestSection.tsx
+++ src/components/RequestSection.tsx
@@ -21,15 +21,17 @@ function RequestSection(myProps: myRequestProps) {
     return (
         <div style={{background: 'lightgrey'}}>
             <Container>
+                {/* Title */}
                 <div className='dashboard-request-section'>
                     <Row style={{ paddingBottom: '20px' }}>
                         <Col className='title ms-auto'>
                             <h1> REQUESTS</h1>
                         </Col >
-
                     </Row>
 
+                        {/*All requests go into a scrollable div  */}
                         <div className='request-container scroll'  style={myProps.requests.length ===0 ? { height:'auto'} : {height : '450px'}}  >
+                            {/* If there are requests, map each element to a div */}
                             {myProps.requests.length !== 0 ? (
                                 myProps.requests.map((requests, index) => (
                                     <div style={index !==0 ? {borderTop: '1px black solid'} : {}}>
@@ -52,14 +54,12 @@ function RequestSection(myProps: myRequestProps) {
 
                                 ))
                             ) : (
+                                // Otherwise, display empty text
                                 <div className='empty'>
                                     <h4> <i>No pages are available </i></h4>
                                 </div>
                             )}
                     </div>
-
-
-
                 </div>
             </Container >
 
