ddd4ecf52f11839a2e808b124fa1e1f7a6c8da9e
Brianna Van Hecke <vanheckeb@merrimack.edu>
2023-10-26 17:07:44 -0400

    Fixing borders, fixed page section

--- src/components/PageSection.tsx
+++ src/components/PageSection.tsx
@@ -16,17 +16,14 @@ interface myPageProps {
 // Returns the page section of the dashboard
 function PageSection(myProps: myPageProps) {
     return (
-        <div>
-            <Container>
-                {/* Title and Create New Project Button */}
+        <div style={{ background: 'lightgrey' }}>
+            <Container >
+                {/* Title */}
                 <div className='dashboard-page-section'>
                     <Row style={{ paddingBottom: '20px' }}>
-                        <Col className='title' md={6} xs={6}>
+                        <Col className='title mx-auto'>
                             <h1> YOUR PAGES</h1>
                         </Col >
-                        <Col className='create-new-button' md={6} xs={6}>
-                            <Button> Create New Project</Button>
-                        </Col>
                     </Row>
 
                     {/* Actual pages */}
@@ -34,14 +31,14 @@ function PageSection(myProps: myPageProps) {
                         {/* If there are pages in the page array, then iterate through and map each element to a new page section */}
                         {myProps.pages.length !== 0 ? (
                             myProps.pages.map((page) => (
-                                <div style={{borderTop: '1px black solid', borderBottom: '1px black solid'}}>
+                                <div style={{ borderTop: '1px black solid' }}>
                                     <Row className='rows'>
-                                        <Col md={11} xs={10} className='page-name'>
+                                        <Col md={11} xs={9} className='page-name'>
                                             <h3>
                                                 {page.pageName}
                                             </h3>
                                         </Col>
-                                        <Col md={1} xs={2} className='new-page-button'>
+                                        <Col md={1} xs={3} className='new-page-button'>
                                             <a href={page.pageLink}>
                                                 <Button className='link-button'><i className="bi bi-arrow-right"></i></Button>
                                             </a>
@@ -52,12 +49,18 @@ function PageSection(myProps: myPageProps) {
                         )
                             : (
                                 // If there are no pages then display empty message
-                                <div className='empty mx-auto'>
+                                <div className='empty mx-auto' style={{ borderTop: '1px black solid' }}>
                                     <h4> <i>No pages are available </i></h4>
                                 </div>
                             )}
-
                     </div>
+
+                    {/* New Project Button */}  
+                    <Row style={{ paddingTop: '20px' }}>
+                        <Col className='create-new-button mx-auto'>
+                            <Button> Create New Project</Button>
+                        </Col>
+                    </Row>
 
                 </div>
             </Container >
