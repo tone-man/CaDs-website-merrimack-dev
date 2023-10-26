99a857d6cf0a8940e8e7611155d6ab38d5497c0a
tones <thetoneman@outlook.com>
2023-10-26 13:42:39 -0400

    Created Firebase object.

---
+++ src/firebase/index.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+const FireBaseApp = initializeApp(firebaseConfig);
+
+// I think authentication needs to go here w/ observer so it is sitewide
+// Just a hunch tho
+
+export default FireBaseApp;
