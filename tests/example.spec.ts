8eaf1c038dd70bb11458c0b70705aa7a19a9275e
Brianna Van Hecke <vanheckeb@merrimack.edu>
2023-10-28 17:15:04 -0400

    Commented out hopefully unnecesary test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,3 +6,11 @@ test('Has Website in Title', async ({ page }) => {
   await expect(page).toHaveTitle("CaDS Website");
 });
 
+// test('Button Increments on Press', async({ page }) => {
+//   await page.goto('/');
+
+//   const button = await page.getByRole('button', {name: 'count'});
+//   await button.click();
+
+//   await expect( await button.textContent()).toContain('1')
+// });
