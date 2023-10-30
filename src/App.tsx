8a177f694736353856c1ab22d270f85784dd196c
tones <thetoneman@outlook.com>
2023-10-30 13:38:20 -0400

    Fixed Bugged Example. Removed Bad Code

--- src/App.tsx
+++ src/App.tsx
@@ -41,18 +41,6 @@ function App() {
                     <Route path='/dashboard' element={<Dashboard />} />
                 </Routes>
                 <Footer />
-                <div className="card">
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p>
             </Router>
         </AuthContext.Provider>
     )
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,15 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Has Website in Title', async ({ page }) => {
   await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Website/);
+  await expect(page).toHaveTitle("CaDS Website");
 });
 
-test('Button Increments on Press', async({ page }) => {
-  await page.goto('/');
-
-  const button = await page.getByRole('button', {name: 'count'});
-  await button.click();
-
-  await expect( await button.textContent()).toContain('1')
-});
