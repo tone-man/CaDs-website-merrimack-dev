53c28edf508a2b23ff3f69b5fec7f6df21b25019
Brianna Van Hecke <vanheckeb@merrimack.edu>
2023-11-05 15:32:01 -0500

    can now edit database from text component

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -1,53 +1,60 @@
+import React, { useEffect, useState } from 'react';
 import { getDatabase, ref, onValue, set } from 'firebase/database';
-import path from 'path';
-import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
-
 const EditPage = () => {
-
     const location = useLocation();
     const { pathName } = location.state as { pathName: string };
+    const [editableComponents, setEditableComponents] = useState('');
     const [snapshotTemp, setSnapshot] = useState({});
-
+    
     // Gets the project information from the database
     useEffect(() => {
         const db = getDatabase();
         const projects = ref(db, 'pages/homepage/components/0/projectList/Automated Lyric Analysis/');
         onValue(projects, (snapshot) => {
             setSnapshot(snapshot.val());
-            console.log(snapshot.val())
+            console.log(snapshot.val());
         });
     }, []);
 
+    // Parse database information and update local state
+    useEffect(() => {
+        const jsonString = JSON.stringify(snapshotTemp, null, 2);
+        console.log(jsonString);
+        setEditableComponents(jsonString);
+    }, [snapshotTemp]);
 
-    // Actually parses database information so it can be passed to other components
-    // useEffect(() => {
-
-
-    // }, [snapshotTemp]);
-
-
-    // https://firebase.google.com/docs/database/web/read-and-write
-    function editDatabase(path: string) {
-        console.log("in here")
+     // https://firebase.google.com/docs/database/web/read-and-write
+     function editDatabase(path: string, data: string) {
         const db = getDatabase();
-        set(ref(db, 'pages/homepage/components/0/projectList/Automated Lyric Analysis/'), {
-            title: path
-        });
+        set(ref(db, path), JSON.parse(data));
     }
 
-    // https://stackoverflow.com/questions/64419526/how-to-delete-a-node-in-firebase-realtime-database-in-javascript
-    function deleteFromDatabase(path: string) {
+       // https://stackoverflow.com/questions/64419526/how-to-delete-a-node-in-firebase-realtime-database-in-javascript
+       function deleteFromDatabase(path: string) {
         const db = getDatabase();
         const chatRef = ref(db,path);
         set(chatRef, null);
     }
 
+    // Update local state when the text area content changes
+    const handleTextAreaChange = (event) => {
+        setEditableComponents(event.target.value);
+    };
+
+    // Save changes back to the database
+    const handleSaveButtonClick = () => {
+        editDatabase('pages/homepage/components/0/projectList/Automated Lyric Analysis/', editableComponents);
+    };
+
+
     return (
         <div>
             <h1>Edit Page</h1>
             <h1> {pathName} </h1>
+            <textarea cols={200} value={editableComponents} onChange={handleTextAreaChange} />
+            <button onClick={handleSaveButtonClick}>Save</button>
         </div>
     );
 };
